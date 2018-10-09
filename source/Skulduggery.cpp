856b633e1819caf292d61c81e7f5ab155d1bf179
octopusarms <octopusagentlu@gmail.com>
2018-10-09 02:35:41 -0500

    Initial commit of Stage 1.

---
+++ source/Skulduggery.cpp
@@ -0,0 +1,16 @@
+#include "Skulduggery.hpp"
+include <iostream>
+
+
+
+
+Skulduggery.Baugette = "This is the start of Skulduggery Engine."
+
+int main( int argc, char* [] args ) 
+{
+
+   iostream.cout << Skulduggery.Baugette;
+
+   return 0;
+
+}
